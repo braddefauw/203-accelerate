ee1f8d852571b89be18ff33e615e757ad18a7152
brad_d <badefauw@gmail.com>
2021-04-14 11:59:39 -0500

    added 404 page and enqued google fonts

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found)
+ */
+get_header();
+ ?>
+
+
+<div id="primary" class="fourohfour-page content-area">
+	<div id="content" class="site-content" role="main">
+		<div class="page-wrapper">
+			<div class="fourohfour-page-content">
+				<h1>404 ERROR - PAGE NOT FOUND!</h1>
+				<h2>"Not all those who wander are lost." - J.R.R. Tolkien</h2>
+				<p>...but you might be</p>
+			</div><!-- .page-content -->
+		</div><!-- .page-wrapper -->
+
+	</div><!-- #content -->
+</div><!-- #primary -->
+
+<?php get_footer(); ?>
+
